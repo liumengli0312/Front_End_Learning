90ad455a25f5436701ddd58201449bd307f719ff
liumengli0312 <38094060+liumengli0312@users.noreply.github.com>
2018-04-27 15:56:30 +0800

    正确打开方式
    在命令窗口，cd进文件夹所在路径目录，输入live-server,回车

---
+++ 20180427/js/script.js
@@ -0,0 +1,43 @@
+app.controller('MyCtrl',function($scope){
+	$scope.book = [{
+		name:"书名：百年孤独",
+		author:"作者：加西亚·马尔克斯",
+		introduction:"简介：作品描写了布恩迪亚家族七代人的传奇故事，以及加勒比海沿岸小镇马孔多的百年兴衰，反映了拉丁美洲一个世纪以来风云变幻的历史。",
+		img:"images/book0.jpg"
+	},
+	{
+		name:"书名：时间简史",
+		author:"作者：斯蒂芬·霍金",
+		introduction:"简介：全书共十二章，讲的全都是关于宇宙本性的最前沿知识，包括：我们的宇宙图像、空间和时间、膨胀的宇宙、不确定性原理、黑洞、宇宙的起源和命运等内容，深入浅出地介绍了遥远星系、黑洞、粒子、反物质等知识，并对宇宙的起源、空间和时间以及相对论等古老命题进行了阐述。",
+		img:"images/book1.jpg"
+	},
+	
+	 {
+		name:"书名：红楼梦",
+		author:"作者：曹雪芹",
+		introduction:"简介：小说以贾、史、王、薛四大家族的兴衰为背景，以富贵公子贾宝玉为视角，描绘了一批举止见识出于须眉之上的闺阁佳人们的人生百态，展现了正邪两赋有情人的人性美和悲剧美。",
+		img:"images/book2.jpg"
+	},
+
+	{
+		name:"书名：水浒传",
+		author:"作者：施耐庵",
+		introduction:"简介：全书通过描写梁山好汉反抗欺压、水泊梁山壮大和受宋朝招安，以及受招安后为宋朝征战，最终消亡的宏大故事。",
+		img:"images/book3.jpg"
+	},
+
+	{
+		name:"书名：JavaScript权威指南",
+		author:"作者：弗拉纳根",
+		introduction:"简介：本书全面介绍了JavaScript语言的核心，以及Web浏览器中实现的遗留和标准的DOM。",
+		img:"images/book4.jpg"
+	},
+
+	{
+		name:"书名：Node.Js开发指南",
+		author:"作者：BYVoid",
+		introduction:"简介：本书首先简要介绍Node.js，然后通过各种示例讲解Node.js 的基本特性，再用案例式教学的方式讲述如何用Node.js 进行Web 开发，接着探讨一些Node.js 进阶话题，最后展示如何将一个Node.js 应用部署到生产环境中。",
+		img:"images/book5.jpg"
+	}];
+
+});
--- kaifanla/js/script.js
+++
@@ -1,9 +0,0 @@
-app.controller("myCtrl", function($scope, dishes) {
-    console.log(dishes[0]);
-    // $scope.dish = dishes;
-    dishes.then(function(res) {
-        $scope.dish = res.data;
-    }, function(error) {
-        console.log(error);
-    });
-});
